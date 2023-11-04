d0b8c85313d9fcbf6c49409fb133e017dc8de308
Hizar62 <hizarsajjad632@gmail.com>
2023-11-04 14:58:36 +0500

    main screen

--- lib/screen/weather_screen.dart
+++ lib/screen/weather_screen.dart
@@ -11,17 +11,49 @@ class WeatherScreen extends StatefulWidget {
 
 class _WeatherScreenState extends State<WeatherScreen> {
   // api key
-  final _WeatherServices = WeatherService(apikey);
+  final _WeatherServices = WeatherService('0e1bfece6d6c691aa78ab9f3009c68ce');
   Weather? _weather;
+
   // fetch weather
+  _fetchWeather() async {
+    String cityName = await _WeatherServices.getCurrentCity();
+
+    try {
+      final weather = await _WeatherServices.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   // weather animation
+
+  @override
+  void initState() {
+    super.initState();
+    // on Start Fetching Weather
+    _fetchWeather();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Weather App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // City
+            Text(_weather?.cityName ?? "Loading City .."),
+
+            // Temperatre
+            Text('${_weather?.temperature.round()}C')
+          ],
+        ),
       ),
     );
   }
