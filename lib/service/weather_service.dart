ca0ba9d97428ae7fd71c9ed0d154c0e4d84bf606
Hizar62 <hizarsajjad632@gmail.com>
2023-11-04 14:21:32 +0500

    Weather Services

---
+++ lib/service/weather_service.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'dart:js_interop_unsafe';
+import 'dart:js_util';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:weatherapp/models/weather_model.dart';
+
+class WeatherService {
+  static const BASE_URL =
+      'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={0e1bfece6d6c691aa78ab9f3009c68ce}';
+  final String apikey;
+
+  WeatherService(this.apikey);
+
+  // Get Weather
+  Future<Weather> getWeather(String cityName) async {
+    final Response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apikey&units=metric'));
+
+    if (Response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(Response.body));
+    } else {
+      throw Exception("Unable to Load Weather");
+    }
+  }
+
+  // Getting Current City
+  Future<String> getCurrentCity() async {
+    // Permission from the user
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    // getting current position
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // convert location into list of placemark objects
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    String? city = placemarks[0].locality;
+    return city ?? "";
+  }
+}
